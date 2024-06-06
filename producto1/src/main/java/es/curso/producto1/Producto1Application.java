e2fbc5009e3938fc59bad54eed8c104514c9e3c5
cursos <cursos@host.docker.internal>
2024-06-06 12:54:11 +0200

    revision

--- producto1/src/main/java/es/curso/producto1/Producto1Application.java
+++ producto1/src/main/java/es/curso/producto1/Producto1Application.java
@@ -1,10 +1,8 @@
 package es.curso.producto1;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class Producto1Application implements CommandLineRunner{
