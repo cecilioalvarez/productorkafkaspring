6ba7ba301e6334469578c8c3a6ccea547fee33fa
cursos <cursos@host.docker.internal>
2024-06-06 11:51:53 +0200

    nuevo

--- producto1/src/main/java/es/curso/producto1/Producto1Application.java
+++ producto1/src/main/java/es/curso/producto1/Producto1Application.java
@@ -9,10 +9,6 @@ import org.springframework.kafka.core.KafkaTemplate;
 @SpringBootApplication
 public class Producto1Application implements CommandLineRunner{
 
-	@Autowired
-	private KafkaTemplate<String,String> kafkaTemplate;
-	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(Producto1Application.class, args);
 	}
@@ -20,9 +16,8 @@ public class Producto1Application implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("hola se ejecuta");
-		String topic= "examen-events";
-		kafkaTemplate.send(topic,"hola desde spring");
+		System.out.println("hola se ejecuta el servidor");
+	
 		
 	}
 
--- producto1/src/main/java/es/curso/producto1/controllers/HolaController.java
+++ producto1/src/main/java/es/curso/producto1/controllers/HolaController.java
@@ -1,14 +1,23 @@
 package es.curso.producto1.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HolaController {
 
-	@RequestMapping("/hola")
-	public String hola() {
-		
-		return "hola";
+
+	@Autowired
+	private KafkaTemplate<String,String> kafkaTemplate;
+	
+	
+	@RequestMapping("/enviar")
+	public String enviar(@RequestParam String mensaje) {
+		String topic= "examen-events";
+		kafkaTemplate.send(topic,mensaje);
+		return "mensaje enviado";
 	}
 }
